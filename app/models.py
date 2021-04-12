9856cef6dceba6a9b72cecc1a5ccfabe1834e39d
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2021-04-12 10:22:09 -0500

    Added models

--- app/models.py
+++ app/models.py
@@ -1,31 +1,75 @@
 from . import db
 
-class Property(db.Model):
+class Users(db.Model):
+    __tablename__ = 'Users'
+ 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(1024))
+    password = db.Column(db.String(255))
+    name = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    location = db.Column(db.String(255))
+    biography = db.Column(db.String(255)) 
+    photo = db.Column(db.String(255))
+    date_joined = db.Column(db.DateTime(timezone=True))
+
+    def __init__(self, username, password, name, email, location, biography, photo, date_joined):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.email = email
+        self.location = location
+        self.biography = biography
+        self.photo = photo
+        self.date_joined = date_joined
+
+    def __repr__(self):
+        return '<User %r, %r>' % (self.id, self.name) 
+
+class Cars(db.Model):
     # You can use this to change the table name. The default convention is to use
     # the class name. In this case a class name of UserProfile would create a
     # user_profile (singular) table, but if we specify __tablename__ we can change it
     # to `user_profiles` (plural) or some other name.
-    # __tablename__ = 'property'
+    __tablename__ = 'Cars'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
     description = db.Column(db.String(1024))
-    rooms = db.Column(db.String(3))
-    bathrooms = db.Column(db.String(3))
-    price = db.Column(db.String(50))
-    ptype = db.Column(db.String(30))
-    location = db.Column(db.String(4096))
+    make = db.Column(db.String(255))
+    model = db.Column(db.String(255))
+    colour = db.Column(db.String(255))
+    year = db.Column(db.String(255))
+    transmission = db.Column(db.String(255)) 
+    car_type = db.Column(db.String(255))
+    price = db.Column(db.Float)
     photo = db.Column(db.String(255))
+    user_id = db.Column(db.Integer)
 
-    def __init__(self, title, description, rooms, bathrooms, price, ptype, location, photo):
-        self.title = title
+    def __init__(self,description, make, model, colour, year, transmission, car_type, price, photo, user_id):
         self.description = description
-        self.rooms = rooms
-        self.bathrooms = bathrooms
+        self.make = make
+        self.model = model
+        self.colour = colour
+        self.year = year
+        self.transmission = transmission
+        self.car_type = car_type
         self.price = price
-        self.ptype = ptype
-        self.location = location
         self.photo = photo
+        self.user_id = user_id
 
     def __repr__(self):
-        return '<Property %r, $%r>' % (self.title, self.price)
+        return '<Car %r, %r %r>' % (self.id, self.make, self.model)
+
+class Favourites(db.Model):
+    __tablename__ = 'Favourites'
+
+    id = db.Column(db.Integer, primary_key=True)
+    car_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+
+    def __init__(self, car_id, user_id):
+        self.car_id = car_id
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<Favourites %r>' % (self.id)
