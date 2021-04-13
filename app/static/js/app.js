82ee4d1228b5a50da80e295d8f22e450a221c9e6
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2021-04-13 12:55:37 -0500

    Styled login page

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -104,9 +104,17 @@ app.component('app-footer', {
 const Home = {
   name: 'Home',
   template: `
-  <div class="jumbotron">
-      <h1>Project 2</h1>
-  </div>
+    <section id="home">
+        <article>
+            <div>
+              <h1 class="font-weight-bold display-4">Buy and Sell Cars Online</h1><br>
+              <p>United Auto Sales provides the fastest, easiest and most user friendly way to buy or sell cars online. Find a Great Price on the Vehicle You Want</p><br>
+              <button type="button" class="btn btn-primary">Register</button>
+              <button type="button" class="btn btn-success">Login</button>
+            </div>
+        </article>
+        <img src="./static/images/car.jpg" class="img-fluid">
+    </section>
   `,
   data() {
       return {}
