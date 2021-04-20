f53ebcd0491d81b34ea44b3a6c611a4fc4bf2b0e
Javon Ellis <javon.ellis@aye-tech.com>
2021-04-20 06:38:32 -0500

    Created and render public appbar and private appbar Explore Page Created

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -10,10 +10,102 @@ const app = Vue.createApp({
 
 });
 
-app.component('app-header', {
-  name: 'AppHeader',
+
+
+app.component('app-footer', {
+  name: 'AppFooter',
   template: `
-  <nav v-if="!isPublic" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  `,
+  data() {
+      return {
+          year: (new Date).getFullYear()
+      }
+  }
+});
+
+const privateAppBar = {
+    name: 'app-bar',
+    template: `  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="/">
+      <svg version="1.1" id="car_logo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+      width="20px" height="20px" viewBox="0 0 39.055 39.054" style="enable-background:new 0 0 39.055 39.054;"
+      xml:space="preserve">
+      <g>
+        <g>
+          <path d="M38.831,14.26c-0.191-0.233-0.476-0.369-0.775-0.369h-3.801c-0.938-2.474-2.16-4.898-3.549-5.813
+            c-4.805-3.161-17.55-3.161-22.355,0c-1.39,0.916-2.607,3.343-3.55,5.813H1c-0.302,0-0.586,0.136-0.775,0.369
+            c-0.19,0.232-0.266,0.539-0.204,0.834l0.563,2.728c0.096,0.465,0.506,0.797,0.979,0.797h1.126
+            c-1.087,1.254-1.614,2.833-1.621,4.413c-0.007,1.952,0.734,3.716,2.089,4.964c0.015,0.013,0.03,0.022,0.044,0.035v3.817
+            c0,0.827,0.672,1.5,1.5,1.5h3.506c0.828,0,1.5-0.673,1.5-1.5v-1.534h19.641v1.534c0,0.827,0.672,1.5,1.5,1.5h3.506
+            c0.826,0,1.5-0.673,1.5-1.5v-3.742c1.438-1.317,2.125-3.129,2.134-4.938c0.006-1.634-0.545-3.271-1.696-4.551h1.201
+            c0.475,0,0.885-0.332,0.979-0.798l0.564-2.727C39.094,14.799,39.021,14.494,38.831,14.26z M9.998,10.583
+            c3.83-2.521,15.229-2.521,19.057,0c0.744,0.488,1.701,2.461,2.578,4.877H7.422C8.297,13.045,9.254,11.073,9.998,10.583z
+              M5.512,23.408c0-1.63,1.322-2.95,2.951-2.95c1.631,0,2.951,1.32,2.951,2.95s-1.32,2.951-2.951,2.951
+            C6.834,26.359,5.512,25.038,5.512,23.408z M30.631,26.359c-1.629,0-2.951-1.321-2.951-2.951s1.322-2.95,2.951-2.95
+            c1.631,0,2.951,1.32,2.951,2.95S32.26,26.359,30.631,26.359z"/>
+        </g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      <g>
+      </g>
+      </svg>
+      <p>United Auto Sales</p>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto ">
+        <li class="nav-item active ml-5">
+          <router-link class="nav-link" to="/">Add Car <span class="sr-only">(current)</span></router-link>
+        </li>
+        <li class="nav-item active ml-5">
+        <router-link class="nav-link" to="/uploads">Explore <span class="sr-only">(current)</span></router-link>
+      </li>
+      <li class="nav-item active ml-5">
+        <router-link class="nav-link" to="/uploads">My Profile <span class="sr-only">(current)</span></router-link>
+      </li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <router-link class="nav-link" to="/login">Logout<span class="sr-only">(current)</span></router-link>
+        </li> 
+      </ul> 
+    </div>
+  </nav>`
+}
+
+const publicAppBar = {
+    name: 'app-bar',
+    template: `  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <a class="navbar-brand" href="/">
       <svg version="1.1" id="car_logo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
       width="20px" height="20px" viewBox="0 0 39.055 39.054" style="enable-background:new 0 0 39.055 39.054;"
@@ -88,30 +180,29 @@ app.component('app-header', {
         </li> 
       </ul> 
     </div>
-  </nav>
+  </nav>`
+}
+
+app.component('app-header', {
+  name: 'AppHeader',
+  template: ` <privateAppBar  v-if="!isPublic" ></privateAppBar>
+ <publicAppBar v-if="isPublic"></publicAppBar>
   `,
+    components:{
+        privateAppBar,
+        publicAppBar
+    },
     data(){
       return {
-          publicRoutes:['Login']
+          publicRoutes:['Login', 'Register']
       }
     },
     computed: {
       isPublic() {
           //Todo change this to an array for multiple public routes (if needed)
-          return this.$route.name  == "Login"
+          return this.publicRoutes.includes(this.$route.name)
       }
     }
-});
-
-app.component('app-footer', {
-  name: 'AppFooter',
-  template: `
-  `,
-  data() {
-      return {
-          year: (new Date).getFullYear()
-      }
-  }
 });
 
 const Home = {
@@ -573,6 +664,93 @@ const ViewCar = {
   }
 }
 
+const CardCarsList = {
+    name: 'card-cars-list',
+    template:`<div class="container">
+<div class="row">
+            <div v-for=" card in cars" class="card col-sm vehicle-list-card" style="width: 18rem;">
+              <img class="card-img-top" :src="card.picture" >
+              <div class="card-body">
+                <h5 class="card-title">{{card.year + " " + card.make}} <span><button class="price-chip "><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag" viewBox="0 0 16 16">
+  <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z"/>
+  <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z"/>
+</svg> {{card.price}}</button></span></h5>
+                <p class="card-text">{{card.model}}.</p>
+                <a href="#" class="btn btn-primary btn-block">View more details</a>
+              </div>
+            </div> 
+            </div>
+</div>`,
+     data: function(){
+      return {
+         cars:[
+             {
+                 year:2020,
+                 make:"Lamborghini",
+                 model:"Huracan",
+                  price:"555,222",
+                 picture:".././images/default-car.jpg"
+             }
+             ,{
+                 year:2020,
+                 make:"Bugatti",
+                 model:"Chiron",
+                  price:"555,222",
+                picture:".././images/default-car.jpg"
+             }
+             ,{
+                 year:2018,
+                 make:"Tesla",
+                 model:"Model S",
+                 price:"555,222",
+                 picture:".././images/default-car.jpg"
+             }
+             ]
+      }
+  }
+}
+
+const ExploreComponent = {
+    name:'explore-component',
+    template:`
+      <section id="explore-page" >
+       <div class="explore-div">
+       <h3 class="font-weight-bold display-5">
+          Explore
+        </h3>
+       <form class="container" @submit.prevent="addCar" method="POST" enctype="multipart/form-data">
+            <div class="form-row  container">
+                <div class="col-sm form-group">
+                    <label for="make">Make</label>
+                    <input class="form-control" id="make" type="text" name="make" placeholder="Tesla">
+                </div>
+                <div class="form-group col-sm">
+                    <label for="model">Model</label>
+                    <input class="form-control" id="model" type="text" name="model" placeholder="Model S">
+                </div> 
+                <div class="form-group col-sm ">
+               
+                    <button type="submit" class="btn-class mt-lg-4">Save</button>
+                </div>
+                   
+                </div> 
+            </div>
+        </form>
+        <CardCarsList></CardCarsList>
+</div>
+
+  </section>
+    `,
+    created(){
+    document.body.classList.add("grey-background");
+  },
+    components : {
+        CardCarsList
+    }
+
+
+}
+
 const NotFound = {
   name: 'NotFound',
   template: `
@@ -594,6 +772,7 @@ const routes = [
   {path:"/register", component: Register, name:"Register"},
   {path:"/cars/new", component: NewCar, name:"NewCar"},
   {path:"/cars/:id", component: ViewCar, name:"ViewCar"},
+  {path:"/cars/explore", component: ExploreComponent, name:"Explore"},
 
   // This is a catch all route in case none of the above matches
   { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound }
