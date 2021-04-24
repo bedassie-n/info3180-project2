7bc81e5db5ad999fe09668b91efd02023df01e7c
Joanna-Joy Gray <jjgray21@hotmail.com>
2021-04-23 21:35:13 -0500

    Register route working on curl | but not postman

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
+
 from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-csrf = CSRFProtect(app)
+#csrf = CSRFProtect(app)
 app.config.from_object(Config)
+#api = restful.Api(app, decorators=[csrf.exempt])
+#app.config['JWT_COOKIE_CSRF_PROTECT'] = False
 
 db = SQLAlchemy(app)
 
 from app import views, models
+from app.models import Users, Favourites, Cars
--- app/views.py
+++ app/views.py
@@ -5,14 +5,91 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 import os
+from datetime import datetime, timezone
 from app import app, db
-from flask import render_template, request, redirect, url_for, flash, send_from_directory
+from flask import render_template, request, redirect, url_for, flash, send_from_directory, abort, jsonify, g, make_response
 from werkzeug.utils import secure_filename
 from app.models import Users, Favourites, Cars
+
+
+"""     JWT     """
+""" def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    auth = request.headers.get('Authorization', None) # or request.cookies.get('token', None)
+    if not auth:
+      return jsonify({'code': 'authorization_header_missing', 'description': 'Authorization header is expected'}), 401
+    parts = auth.split()
+    if parts[0].lower() != 'bearer':
+      return jsonify({'code': 'invalid_header', 'description': 'Authorization header must start with Bearer'}), 401
+    elif len(parts) == 1:
+      return jsonify({'code': 'invalid_header', 'description': 'Token not found'}), 401
+    elif len(parts) > 2:
+      return jsonify({'code': 'invalid_header', 'description': 'Authorization header must be Bearer + \s + token'}), 401
+    token = parts[1]
+    try:
+        payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+    except jwt.ExpiredSignatureError:
+        return jsonify({'code': 'token_expired', 'description': 'token is expired'}), 401
+    except jwt.DecodeError:
+        return jsonify({'code': 'token_invalid_signature', 'description': 'Token signature is invalid'}), 401
+    g.current_user = user = payload
+    return f(*args, **kwargs)
+  return decorated """
 
 ###
 # Routing for your application.
 ###
+"""              API: AUTHENTICATION              """
+
+# This route doesn't require a JWT
+@app.route('/api/register', methods=['POST'])
+def register(): 
+    # if the request data is not there
+    # or any of the username/password/name/email is missing
+    # respond with a bad request code and stop registration
+    # if not request.json: # or not 'username' in request.json or not 'password' in request.json or not 'name' in request.json or not 'email' in request.json:
+    #    abort(400) #bad request http code
+    #if not request.form: # or not 'username' in request.form or not 'password' in request.form or not 'name' in request.form or not 'email' in request.form:
+     #  abort(400) #bad request http code
+
+    if request.form:
+        # create user 
+        user = Users (
+            username = request.form['username'],
+            password = request.form['password'], 
+            name = request.form['name'],
+            email = request.form['email'],
+            location = request.form.get('location', ""),
+            biography = request.form.get('biography', ""),
+            photo = request.form.get('photo', ""),
+            date_joined =  datetime.now(timezone.utc)
+        )
+
+        #add user to db
+        db.session.add(user)
+        db.session.commit()
+
+        #get the user from the db
+        newUser = Users.query.filter_by(username = request.form['username']).first()
+
+        #build api response with user data
+        userResult = {
+            'id': newUser.id, 
+            'username': newUser.username,
+            'name': newUser.name,
+            'email': newUser.email,
+            'location': newUser.location,
+            'biography': newUser.biography,
+            'photo': newUser.photo,
+            'date_joined': newUser.date_joined
+        }
+
+        #send api response
+        return jsonify({'user': userResult}), 201
+    else:
+    #     abort(400) #bad request http code
+          return jsonify({'user': []}), 200
 
 # Please create all new routes and view functions above this route.
 # This route is now our catch all route for our VueJS single page
@@ -84,4 +161,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port="8080")
+    app.run(debug=True, host="127.0.0.1", port="8079")
