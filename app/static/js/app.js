1c08b5887fa05f076a1c6692d0ddab02c54c4958
Javon Ellis <javon.ellis@aye-tech.com>
2021-04-26 19:31:06 -0500

    Changed navbar on default home page and remove token on logout

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -104,11 +104,16 @@ const privateAppBar = {
       </ul>
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <router-link class="nav-link" to="/login">Logout<span class="sr-only">(current)</span></router-link>
+          <router-link @click="logout" class="nav-link" to="/">Logout<span class="sr-only">(current)</span></router-link>
         </li> 
       </ul> 
     </div>
-  </nav>`
+  </nav>`,
+    methods : {
+        logout() {
+            localStorage.removeItem("token");
+        }
+    }
 }
 
 const publicAppBar = {
@@ -202,7 +207,7 @@ app.component('app-header', {
     },
     data(){
       return {
-          publicRoutes:['Login', 'Register']
+          publicRoutes:['Login', 'Register', 'Home']
       }
     },
     computed: {
@@ -1065,7 +1070,7 @@ const NotFound = {
 
 // Define Routes
 const routes = [
-  { path: "/", component: Home },
+  { path: "/", component: Home, name:"Home" },
   // Put other routes here
   {path:"/login",component: loginComponent, name:"Login", props: true},
   {path:"/register", component: Register, name:"Register", props:true},
